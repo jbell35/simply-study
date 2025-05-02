e7ebb031284be78df336f4a79f95548cf1328d2d
jbell35 <134554290+jbell35@users.noreply.github.com>
2025-05-01 22:53:16 -0500

    task cards will be added to page when created

--- javascript/home.js
+++ javascript/home.js
@@ -1,23 +1,33 @@
 
+//open form as a popup
 function openForm() {
     document.getElementById("formContainer").style.display = "block";
     document.getElementById("blur").style.display = "block";
 }
 
+//close the form
 function closeForm(){
     document.getElementById("formContainer").style.display = "none";
     document.getElementById("blur").style.display = "none";
 }
 
-
-
-function createCard() {
-    const card = '';
-
-    return card;
-}
-
+//take input from the form and display
 const getTask = () => {
+    const container = document.createElement("div");
+    container.classList = "task-card";
+
+    const tCont = document.createElement("h3");
+    tCont.id = "taskName";
+
+    const subjCont = document.createElement("h4");
+    subjCont.id = "className";
+
+    const descCont = document.createElement("p");
+    descCont.id = "tDesc";
+
+    const dateCont = document.createElement("p");
+    dateCont.id = "tDate";
+
     const task = document.getElementById("taskInput");
     const subject = document.getElementById("subject");
     const descr = document.getElementById("description");
@@ -28,15 +38,22 @@ const getTask = () => {
     descrInput = descr.value;
     dueDateInput = dueDate.value;
 
-    document.getElementById("taskName").textContent = taskInput;
-    document.getElementById("className").textContent = subjectInput;
-    document.getElementById("tDesc").textContent = descrInput;
-    document.getElementById("tDate").textContent = dueDateInput;
+    tCont.textContent = taskInput;
+    subjCont.textContent = subjectInput;
+    descCont.textContent = descrInput;
+    dateCont.textContent = dueDateInput;
     
+    container.appendChild(tCont);
+    container.appendChild(subjCont);
+    container.appendChild(descCont);
+    container.appendChild(dateCont);
+
+    document.getElementById("cardsCont").appendChild(container);
+
 }
 
 const submitBtn = document.getElementById("submitBtn");
-
+//submit button actions
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
     closeForm();
